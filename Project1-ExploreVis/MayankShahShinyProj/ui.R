b79f3f5094ec59df9132fe99603207d8cc491779
mayankshah891 <mayankshah891@gmail.com>
2017-01-30 17:25:51 -0500

    again

--- Project1-ExploreVis/MayankShahShinyProj/ui.R
+++ Project1-ExploreVis/MayankShahShinyProj/ui.R
@@ -1,18 +1,14 @@
-library(shiny)
-shinyUI(fluidPage(
-  headerPanel(
-    h1("NYC Crime Over Time", align = "center")
+
+library(shinydashboard)
+
+shinyUI(dashboardPage(
+  dashboardHeader(title = "NYC Crime"),
+  dashboardSidebar(
+    selectInput("select", label = h5("Select Crime"), choices = list("Assault" = "Assault", "Burglary"="Burglary", "Car Theft" = "Car Theft", "Grand Larceny" = "Grand Larceny", "Murder" = "Murder", "Rape" = "Rape", "Robbery" = "Robbery")),
+    sliderInput("Slider", "Year", 2000, 2015, 2000, step=1, sep = "")
   ),
-  title = "NYC Crime Over Time",
-  plotOutput("plot", width = "82%"),
-  hr(),
-  br(),
-  br(),
-  fluidRow(
-                 mainPanel(align = "center",
-                   selectInput("select", label = h5("Select Crime"),
-                               choices = list("Assault" = "Assault", "Burglary" = "Burglary", "Burglary" = "Burglary", "Grand Larceny" = "Grand Larceny", "Murder" = "Murder", "Rape" = "Rape", "Robbery" = "Robbery", "Car Theft" = "Car Theft")),
-                   sliderInput("Slider", "Year", 2000, 2015, 2000, step=1, sep = "")
-              )
-        )))
+  dashboardBody(
+    plotOutput("plot", width = "82%")
+  )
+))
 
