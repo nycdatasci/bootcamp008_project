05eccc1ec558c391471145d51a76ee412abe7bdd
gdwebb <grantdwebb85@gmail.com>
2017-01-31 21:50:52 -0500

    Moved a part of the code to the global file

--- Project1-ExploreVis/GrantWebb/global.R
+++ Project1-ExploreVis/GrantWebb/global.R
@@ -1,3 +1,25 @@
+library(shiny)
+library(shinydashboard)
+library(googleVis)
+library(ggplot2)
+library(dplyr)
+library(DT)
+
+student_mat_df = read.csv("~/Desktop/Main Folder/NYC_Data_Science/DataScienceProject1/data/student-mat.csv", stringsAsFactors = FALSE)
+student_por_df = read.csv("~/Desktop/Main Folder/NYC_Data_Science/DataScienceProject1/data/student-por.csv", stringsAsFactors = FALSE)
+student_df <- rbind(student_mat_df,student_por_df)
+student_df_norepeats <- student_df %>% distinct(school,sex,age,address,famsize,Pstatus,
+                                                Medu,Fedu,Mjob,Fjob,reason,
+                                                guardian,traveltime,studytime,failures,
+                                                schoolsup, famsup,activities,nursery,higher,internet,
+                                                romantic,famrel,freetime,goout,Dalc,Walc,health,absences, .keep_all = TRUE)
+student_df_norepeats$Dalc <- ordered(student_df_norepeats$Dalc,
+                                     levels = c(1,2,3,4,5), 
+                                     labels = c("very little", "little", "medium","heavy","very heavy"))
+student_df_norepeats$Walc <- ordered(student_df_norepeats$Walc,
+                                     levels = c(1,2,3,4,5), 
+                                     labels = c("very little", "little", "medium","heavy","very heavy"))
+student_df_norepeats$age <- as.factor(student_df_norepeats$age)
 student_df_norepeats$Dalc <- as.factor(student_df_norepeats$Dalc)
 student_df_norepeats$Walc <- as.factor(student_df_norepeats$Walc)
 student_df_norepeats$age <- as.factor(student_df_norepeats$age)
