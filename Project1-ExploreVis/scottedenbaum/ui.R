fc248ec4aab99cce7a3faf8823e13a0be43c8baa
Scott Edenbaum <Scott.Edenbaum@gmail.com>
2017-02-02 09:41:15 -0500

    fix

--- Project1-ExploreVis/scottedenbaum/ui.R
+++ Project1-ExploreVis/scottedenbaum/ui.R
@@ -1,6 +1,4 @@
 library(shinydashboard)
-library(DT)
-mf0 <- read.csv("mf0.csv")
 sel_group_by = c('Individual' = 'Client.Uid',
                  'Corp Location' = 'EE.Provider',
                  'Household Formation' = 'Household.Type',
@@ -29,24 +27,13 @@ shinyUI(dashboardPage(skin="red",
         menuItem("About POH", tabName = "POH", icon = icon("road")),        
         menuSubItem("View Goal Analysis", tabName = "Goal-Geog", icon=icon("bar-chart")),
         menuSubItem("View Goal Classification Analysis", tabName = "Goal-Cat", icon=icon("gear")),
-<<<<<<< HEAD
-=======
-        menuSubItem("Data", tabName = "data", icon = icon("database")),
->>>>>>> c8f59dfdff54e3097561b1d069001a5d20d50fd8
         tags$img(src = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRCdSDxxGEbkNQyzKbrOyKtIMJdPUx90lSvaXqAgaQP3NgOVfl5", width = 300)
         
         )#sidebarmenu 
   ),
   dashboardBody(
         tabItems(
-<<<<<<< HEAD
           
-=======
-          tabItem(tabName = "data",
-                  # datatable
-                  fluidRow(box(DT::dataTableOutput("table", width = "1500px")))
-                  ),          
->>>>>>> c8f59dfdff54e3097561b1d069001a5d20d50fd8
           tabItem(tabName = "Goal-Cat",
                   fluidRow(column(width = 6, plotOutput("rose", width = '220%', height = '850px')))
                   ),
@@ -63,26 +50,25 @@ shinyUI(dashboardPage(skin="red",
                                   #box(imageOutput("image2", width=300, height=300)),
                                   #box(title = "Pathway of Hope",imageOutput("POHLogo", width = 300, height = 300), imageOutput("POHLo"))
                                  HTML('<iframe width="1280" height="720" src="https://www.youtube.com/embed/wTlzxAvUAuU?T=77" frameborder="0" allowfullscreen></iframe>') #tags$embed(src="https://www.youtube.com/embed/wTlzxAvUAuU?t=77")
-                                 
-                                 #box(imageOutput("imageRedShield", width = 300, height = 300))
+                                  #box(imageOutput("imageRedShield", width = 300, height = 300))
                   ),
                   fluidRow(column(10, br(), h1(p(strong("The Salvation Army", style = "color:red; font-family: 'times'; font-si16pt")),align = "center"), h2(p(strong("Pathway of Hope", style = "font-family: 'times'; font-si16pt")), align = "center"),
-                                     h3(    p("Pathway of Hope is an expert solution to families in crisis through The
-                                            Salvation Army. It is targeted and intensive case management to assist
-                                            families striving to break free from intergenerational poverty. The
-                                            Salvation Army forms a crucial partnership with families in need. Families
-                                            participating in the program possess the desire to change their situation, and are
-                                            willing to share accountability with The Salvation Army for planned
-                                            actions. Through achieving increased stability, these families find a newfound
-                                            hope, propelling them forward on their journey to", strong(em("self-sufficiency.", style = 'color:red')), align = "center", style = "font-family: 'times'; font-si16pt"),
-                                           br(),
-                                          p("This analysis takes a deeper look at the initiative, running in
-                                          over 25 local communities within The Salvation Army’s Eastern Territory.
-                                          The in-take process individually evaluates a family in crisis, and
-                                          identifies custom and critical goals ranging from securing employment to
-                                          finding affordable childcare", align = "center", style = "font-family: 'times'; font-si16pt"))
-                                  )
-                          )                 
+                                  h3(    p("Pathway of Hope is an expert solution to families in crisis through The
+                                           Salvation Army. It is targeted and intensive case management to assist
+                                           families striving to break free from intergenerational poverty. The
+                                           Salvation Army forms a crucial partnership with families in need. Families
+                                           participating in the program possess the desire to change their situation, and are
+                                           willing to share accountability with The Salvation Army for planned
+                                           actions. Through achieving increased stability, these families find a newfound
+                                           hope, propelling them forward on their journey to", strong(em("self-sufficiency.", style = 'color:red')), align = "center", style = "font-family: 'times'; font-si16pt"),
+                                         br(),
+                                         p("This analysis takes a deeper look at the initiative, running in
+                                           over 25 local communities within The Salvation Army’s Eastern Territory.
+                                           The in-take process individually evaluates a family in crisis, and
+                                           identifies custom and critical goals ranging from securing employment to
+                                           finding affordable childcare", align = "center", style = "font-family: 'times'; font-si16pt"))
+                                         )
+                                         )                 
                   )#tabitem2
           
               )#tabitems
