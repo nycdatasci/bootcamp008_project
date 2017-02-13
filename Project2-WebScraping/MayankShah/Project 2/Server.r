599ae6de71b21c5de5435dba8e52b3ea62468584
mayankshah891 <mayankshah891@gmail.com>
2017-02-13 10:03:08 -0500

    server update

--- Project2-WebScraping/MayankShah/Project 2/Server.r
+++ Project2-WebScraping/MayankShah/Project 2/Server.r
@@ -14,13 +14,13 @@ library(car)
 
 shinyServer(function(input, output){
   output$SongPlot <- renderPlot({
-    ggplot(FinalScrape, aes(Date, Length, label=Song)) + geom_point(aes(Date, Length), size = 2, color = 'grey') + geom_label_repel(aes(Date, Length, fill = factor(Group), label = Song), fontface='plain', box.padding = unit(.35, "lines"), point.padding = unit(0.5, "lines"), segment.color = 'grey50') + ylim(77, 112) + xlab("") + ylab("Days on Top")
+    ggplot(FinalScrape, aes(Date, Length, label=Song)) + geom_point(aes(Date, Length), size = 2, color = 'grey') + geom_label_repel(aes(Date, Length, fill = factor(Group), label = Song), fontface='plain', box.padding = unit(.35, "lines"), point.padding = unit(0.5, "lines"), segment.color = 'grey50') + ylim(77, 112) + xlab("") + ylab("Days on Top") + ggtitle("Top 20 Songs (by Days at #1)") + theme(plot.title = element_text(lineheight = 6, face = "bold", size = 32, hjust = 0.6)) 
   })
 
   output$BarPlot <- renderPlot({
-    ggplot(AverageTime, aes(Group, Average_Time, fill = as.factor(Group))) + geom_bar(stat = "identity") + xlab("Decade") + ylab("Average Days at #1") + theme(legend.title=element_blank())
+    ggplot(AverageTime, aes(Group, Average_Time, fill = as.factor(Group))) + geom_bar(stat = "identity") + xlab("Decade") + ylab("Average Days at #1") + ggtitle("Average Days Spent at #1, by Decade") + theme(legend.title=element_blank()) + theme(plot.title = element_text(face = "bold", size = 26, hjust = 0.5))
   })
   output$GenrePlot <- renderPlot({
-    ggplot(GenreSummary, aes(x = Group, y = GenreSums, fill = Genre)) + geom_bar(stat = "identity", position = "fill") + xlab("Decade") + ylab("Genre Breakdown")
+    ggplot(GenreSummary, aes(x = Group, y = GenreSums, fill = Genre)) + geom_bar(stat = "identity", position = "fill") + xlab("Decade") + ylab("Genre Breakdown") + ggtitle("Genre Breakdown by Decade") + theme(plot.title = element_text(face = "bold", size = 26, hjust = 0.5))
   })
 })
