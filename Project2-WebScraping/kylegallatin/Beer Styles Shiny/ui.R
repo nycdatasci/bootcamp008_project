02e01c1f675d3eb4718e8a4d89100dacf2081d0f
kylegallatin <kylegallatin@gmail.com>
2017-02-15 09:58:24 -0500

    Project 2

--- Project2-WebScraping/kylegallatin/Beer Styles Shiny/ui.R
+++ Project2-WebScraping/kylegallatin/Beer Styles Shiny/ui.R
@@ -13,21 +13,8 @@ shinyUI(dashboardPage(
       menuItem("Table", tabName = 'table', icon = icon('table')),
       sliderInput("ratings","Min Number of Ratings", 0, 16327, 0),
       sliderInput("avg", "Min Average Rating", 0 , 5, 0, step = 0.1),
-      sliderInput('alcohol', "Min Alcohol Content", 0, 13, 0, step = 0.1),
-      checkboxGroupInput("checkGroup",
-                         label = h3("Select Style"),
-                         choices = list("American Amber" = "American Amber",
-                                        "American Black Ale" = "American Black Ale",
-                                        "American Blonde Ale" = "American Blonde Ale",
-                                        "American Brown Ale" = "American Brown Ale",
-                                        "American Double/Imperial IPA" = "American Double/Imperial IPA",
-                                        "American Double/Imperial Stout" = "American Double/Imperial Stout",
-                                        "American IPA" = "American IPA",
-                                        "American Pale/Wheat Ale" = "American Pale/Wheat Ale",
-                                        "American Pale Ale" = "American Pale Ale",
-                                        "American Porter" = "American Porter",
-                                        "American Stout" = "American Stout"), 
-                         selected = unique(beerStyle$Style))
+      sliderInput('alcohol', "Min Alcohol Content", 0, 13, 0, step = 0.1)
+      
     )),
   dashboardBody(
     tabItems(
@@ -40,5 +27,21 @@ shinyUI(dashboardPage(
               fluidRow(plotOutput("plot3"))),
       tabItem(tabName = 'table',
               fluidRow(dataTableOutput("table1")))
+  ),
+  fluidRow(
+    checkboxGroupInput("checkGroup",
+                       label = h3("Select Style"),
+                       choices = list("American Amber" = "American Amber",
+                                      "American Black Ale" = "American Black Ale",
+                                      "American Blonde Ale" = "American Blonde Ale",
+                                      "American Brown Ale" = "American Brown Ale",
+                                      "American Double/Imperial IPA" = "American Double/Imperial IPA",
+                                      "American Double/Imperial Stout" = "American Double/Imperial Stout",
+                                      "American IPA" = "American IPA",
+                                      "American Pale/Wheat Ale" = "American Pale/Wheat Ale",
+                                      "American Pale Ale" = "American Pale Ale",
+                                      "American Porter" = "American Porter",
+                                      "American Stout" = "American Stout"), 
+                       selected = unique(beerStyle$Style))
   ))
 ))
