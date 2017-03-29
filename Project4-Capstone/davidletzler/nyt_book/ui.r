03063626dbb937ffc3b0f9709c9333c50c90b2ba
dletzler <david.letzler@gmail.com>
2017-03-29 11:30:03 -0400

    Removed General in UI

--- Project4-Capstone/davidletzler/nyt_book/ui.r
+++ Project4-Capstone/davidletzler/nyt_book/ui.r
@@ -11,7 +11,7 @@ shinyUI(dashboardPage(
       menuItem("Bestsellers Over Time", tabName="time", icon=icon("line-chart")),
       selectInput("era", "Select Year Range", choices= c("2008-2013"='pre', "2013-2017"='post')),
       selectInput("list", "Select List", choices = c("All" = "All", "Hardcover Fiction"="Hardcover Fiction", "Trade Paperback Fiction"="Paperback Trade Fiction", "Mass Market Fiction"="Paperback Mass-Market Fiction", "E-Book Fiction"="E-Book Fiction")),
-      selectInput("genre", "Select Imprint Genre", choices = c("All", "Commericial"="Commercial", "General"="General", "Spiritual"="Spiritual", "Literary"="Literary", "Genre" ="Genre", "Science Fiction & Fantasy"="Science Fiction & Fantasy", "Romance/Erotica"="Romance/Erotica", "Mystery/Crime"="Mystery/Crime", "Thriller/Horror"="Thriller/Horror")),
+      selectInput("genre", "Select Imprint Genre", choices = c("All", "Commericial"="Commercial", "Spiritual"="Spiritual", "Literary"="Literary", "Genre" ="Genre", "Science Fiction & Fantasy"="Science Fiction & Fantasy", "Romance/Erotica"="Romance/Erotica", "Mystery/Crime"="Mystery/Crime", "Thriller/Horror"="Thriller/Horror")),
       selectInput("rank", "Select Top Seller Option", choices=c("Imprint"="publisher", "Author"="author", "Title"="title"))
     )
     
